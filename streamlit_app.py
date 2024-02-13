37edec5824b00f047b3e42c00bce6f025ee1e138
Ling Liang <liangling2001@gmail.com>
2024-02-13 10:24:29 -0500

    Completed Problem 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ ages = [
 
 heatmap = alt.Chart(subset).mark_rect().encode(
     alt.X('Age:O', sort=ages),
-    alt.Y('Country:N', sort=alt.EncodingSortField(field='Rate')),
+    alt.Y('Country:N', sort=alt.EncodingSortField(field='Rate', order='descending')),
     alt.Color('Rate', scale=alt.Scale(type='log', domain=(0.01,100), clamp=True), legend=alt.Legend(title='Mortality rate per 100k')),
     tooltip=[alt.Tooltip('Country:N'), alt.Tooltip('Age:O'), alt.Tooltip('Rate:Q', title='Mortality rate',format='.2f')]
 ).properties(
