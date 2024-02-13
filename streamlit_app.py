9035eedcd15077fe3f2d084088ecc537597a4177
Ling Liang <liangling2001@gmail.com>
2024-02-13 09:54:07 -0500

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,15 +85,15 @@ ages = [
     "Age >64",
 ]
 
-# heatmap = alt.Chart(subset).mark_rect().encode(
-#     alt.X('Age:O', sort=ages),
-#     alt.Y('Country:N', sort=alt.EncodingSortField(field='Rate', order='descending')),
-#     alt.Color('Rate', scale=alt.Scale(type='log', domain=(0.01,1000), clamp=True), legend=alt.Legend(title='Mortality rate per 100k')),
-#     tooltip=[alt.Tooltip('Country:N'), alt.Tooltip('Age:O'), alt.Tooltip('Rate:Q', title='Mortality rate',format='.2f')]
-# ).properties(
-#     title=f"{cancer_type} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
-#     height=300
-# )
+heatmap = alt.Chart(subset).mark_rect().encode(
+    alt.X('Age:O', sort=ages),
+    alt.Y('Country:N', sort=alt.EncodingSortField(field='Rate', order='descending')),
+    alt.Color('Rate', scale=alt.Scale(type='log', domain=(0.01,1000), clamp=True), legend=alt.Legend(title='Mortality rate per 100k')),
+    tooltip=[alt.Tooltip('Country:N'), alt.Tooltip('Age:O'), alt.Tooltip('Rate:Q', title='Mortality rate',format='.2f')]
+).properties(
+    title=f"{cancer_type} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+    height=300
+)
 # # st.altair_chart(heatmap, use_container_width=True)
 # # chart = alt.Chart(subset).mark_bar().encode(
 # #     x=alt.X('sum(Pop):Q', title='Sum of population size'),
@@ -104,17 +104,17 @@ ages = [
 # #     height=300
 # # )
 
-chart = alt.Chart(subset).mark_bar().encode(
-    x=alt.X('sum(Pop):Q', title='Sum of population size'),
-    y=alt.Y('Country:N', sort='-x'),
-    tooltip=[alt.Tooltip('Country:N'), alt.Tooltip('sum(Pop):Q', title='Population size')]
-).properties(
-    height=300
-)
+# chart = alt.Chart(subset).mark_bar().encode(
+#     x=alt.X('sum(Pop):Q', title='Sum of population size'),
+#     y=alt.Y('Country:N', sort='-x'),
+#     tooltip=[alt.Tooltip('Country:N'), alt.Tooltip('sum(Pop):Q', title='Population size')]
+# ).properties(
+#     height=300
+# )
 
 ### P2.5 ###
 # combined_chart = alt.vconcat(heatmap, chart)
-st.altair_chart(chart, use_container_width=True)
+st.altair_chart(heatmap, use_container_width=True)
 
 countries_in_subset = subset["Country"].unique()
 if len(countries_in_subset) != len(countries):
